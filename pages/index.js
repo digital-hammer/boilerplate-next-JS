3d3843e9573d02b12d3319f9d776c49bb3c17c37
Ellis Hogan <ellishogan@Elliss-Mac-mini.local>
2021-03-24 21:10:10 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import {useState, useEffect, useRef} from 'react'
 import cx from 'classnames'
 
-import Hero from '../sections/hero'
-import Header from '../sections/header'
-import Content from '../sections/contentSection'
-
 import layout from '../styles/layout.module.scss'
 import global from '../styles/global.module.scss'
 
@@ -22,15 +18,7 @@ export default () => {
 
   return (
     <>
-      <Header width={width} />
-      <Hero animatedV={true}>
-        <div className={cx(layout.f_col, layout.justify_end, layout.align_start, layout.h100_percent, layout.w100_percent, layout.container, global.text_white)}>
-          <h1 ref={(e)=> e && animatedText.push(e)} className={cx(global.animation, global.animate_in_top)}>Treehouse Vineyards</h1>
-          <h2 ref={(e)=> e && animatedText.push(e)} className={cx(global.animation, global.animate_in_left)}>Treehouse Vineyards</h2>
-          <h2 ref={(e)=> e && animatedText.push(e)} className={cx(global.animation, global.animate_in_bottom)}>Treehouse Vineyards</h2>
-        </div>
-      </Hero>
-      <Content />
+      
     </>
   )
 }
